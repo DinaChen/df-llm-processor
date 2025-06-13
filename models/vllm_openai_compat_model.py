af648978a9d4c0477175218bfdab748c158608e3
DinaChen <dinachen21@gmail.com>
2025-06-14 00:51:09 +0800

    Remove prompt from VLLMOpenAICompatModel attributes.

--- models/vllm_openai_compat_model.py
+++ models/vllm_openai_compat_model.py
@@ -1,4 +1,3 @@
-import requests
 from openai import OpenAI
 from typing import Optional, Union
 
@@ -12,18 +11,16 @@ class VLLMOpenAICompatModel():
         api_key: Union[str, None],
         base_url: str,
         model_name: str,
-        prompt: str,
         max_tokens: int = 1000,
         temperature: Optional[float] = 0.9,
     ):
         self.api_key = api_key if api_key else "dummy_dina"
         self.base_url = base_url
         self.model_name = model_name
-        self.prompt = prompt
         self.max_tokens = max_tokens
         self.temperature = temperature
         
-    def get_llm_response(self, query:str):
+    def get_llm_response(self, prompt:str, query:str):
         """
         Sends a prompt to the llm server via vLLM and returns the generated text.
 
@@ -35,18 +32,21 @@ class VLLMOpenAICompatModel():
 
         try:
             client = OpenAI(api_key = self.api_key, base_url = self.base_url)
-
+            
+            messages=[
+                {"role": "system", "content": prompt},
+                {"role": "user", "content": query}
+            ]
+        
             response = client.chat.completions.create(
                 model = self.model_name,
-                messages=[
-                    {"role": "system", "content": self.prompt},
-                    {"role": "user", "content": query}
-                ],
+                messages = messages,
                 max_tokens = self.max_tokens,
                 temperature = self.temperature,
             )
 
             return response.choices[0].message.content
+
         
         except Exception as e:
             return f"Error: {e}"
@@ -57,12 +57,13 @@ if __name__ == "__main__":
     # TODO: put this config to Class attribute
     prompt = "将下列一个或多个国家代号变成国家名字返回，不要有额外解释。"
     query = "ESP, FRA,NLD"
+
     url = "http://localhost:8800/v1"
     model_name = "Qwen2.5-14B-Instruct"
 
 
-    llm = VLLMOpenAICompatModel(None, url, model_name, prompt)
-    output = llm.get_llm_response(query)
+    llm = VLLMOpenAICompatModel(None, url, model_name)
+    output = llm.get_llm_response(prompt, query)
 
     print("Response:", output)
 """
